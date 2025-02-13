0ab2638d1790fbc0b8082bfea094eb8850f5ceed
Anıl Burak Dolaşık <anilburakd05@gmail.com>
2025-02-13 22:50:21 +0300

    update

--- uniscraper/uniscraper/bot.py
+++ uniscraper/uniscraper/bot.py
@@ -4,6 +4,8 @@ from discord import Intents, Client, Message
 import json
 from scrapy.crawler import CrawlerProcess
 from spiders.universityScraper import UniversityscraperSpider
+from twisted.internet import reactor
+from scrapy.crawler import CrawlerRunner
 
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
@@ -17,7 +19,16 @@ bolumler = {"tıp": 10206,
             "tip": 10206,
             "diş": 10049,
             "dis": 10049,
+            "diş-hekimliği": 10049,
             "ceng": 10024,
+            "pc-müh": 10024,
+            "bilgisayar-mühendisliği": 10024,
+            "kimya-mühendisliği": 10127,
+            "kimya-müh": 10127,
+            "fizik-müh": 10071,
+            "fizik-mühendisliği": 10071,
+            "yazılım-müh": 10233,
+            "yazılım-mühendisliği": 10233,
             "eczacılık": 10050
             }
 def read_json():
@@ -32,9 +43,9 @@ async def send_message(message: Message, user_message: str) -> None:
         return
 
     user_message = user_message.split()
-    uni = user_message[1].replace("-", " ")
-    bolum = user_message[2].lower()
     if user_message[0] == '!netler':
+        uni = user_message[1].replace("-", " ")
+        bolum = user_message[2].lower()
         await message.channel.send("Selam")
         await scrape_uni(uni, bolumler[bolum])
         await message.channel.send(read_json())
@@ -55,7 +66,7 @@ async def scrape_uni(uni, bolum):
     )
     process.crawl(UniversityscraperSpider, arananUni=uni, bolumKodu=bolum)
     process.start()
-
+    # Bu ilk yazılan kod
 
 @client.event
 async def on_ready():
@@ -77,3 +88,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    reactor.run()
